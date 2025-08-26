16c6b7361c3499630182f596f7090ac56d950035
zodex-2 <mdbinbillah@gmail.com>
2025-08-26 22:50:39 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["100092278980386"];
+		const permission = ["61579681084940"];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage("You don't have permission to use this command.", event.threadID, event.messageID);
 		}
